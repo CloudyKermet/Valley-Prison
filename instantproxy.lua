54a885658b2b135d760470c373ccef78ab3ba92f
CloudyKermet <gamingbright2011@gmail.com>
2026-06-26 14:12:01 -0500

    Implement highlight for searchable bins
    Add highlight effect to bins in searchable storages.

--- instantproxy.lua
+++ instantproxy.lua
@@ -1,5 +1,13 @@
+local vesp = Instance.new("Highlight")
+vesp.Name = "ESP"
+vesp.FillTransparency = 1
+vesp.OutlineTransparency = 0
+vesp.OutlineColor = Color3.fromRGB(255, 255, 255)
+
 for _, bin in ipairs(workspace.Map.Functional.Storages.Searchable:GetChildren()) do
 if bin.Name == "Bin" then
+    local hl = vesp:Clone()
+    hl.Parent = bin
 bin.Proximity.ProximityPrompt.HoldDuration = 0.01
   end
 end
